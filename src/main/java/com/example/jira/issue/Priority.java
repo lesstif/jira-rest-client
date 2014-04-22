482ede804af3cb9fdccf7c150d0280e8cf11a488
lesstif <lesstif@gmail.com>
2014-04-22 17:45:01 +0900

    Issue implementating.

---
+++ src/main/java/com/example/jira/issue/Priority.java
@@ -0,0 +1,11 @@
+package com.example.jira.issue;
+
+import lombok.Data;
+
+@Data
+public class Priority {
+	private String self;
+	private String iconUrl;
+	private String name;
+	private String id;
+}
---
+++ src/main/java/com/example/jira/issue/Status.java
@@ -0,0 +1,14 @@
+package com.example.jira.issue;
+
+import lombok.Data;
+
+@Data
+public class Status {
+	private String self;
+	private String description;
+	private String iconUrl;
+	private String name;
+	private String id;
+	
+	private StatusCategory statusCategory;
+}
