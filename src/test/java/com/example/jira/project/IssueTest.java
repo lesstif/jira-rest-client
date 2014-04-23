10a64337b14da5cc9156b18ffb48e113f71b6e8c
lesstif <lesstif@gmail.com>
2014-04-23 17:49:43 +0900

    split test case

---
+++ src/test/java/com/example/jira/project/IssueTest.java
@@ -0,0 +1,42 @@
+package com.example.jira.project;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.example.jira.issue.IssueService;
+import com.example.jira.issue.Issue;
+
+public class IssueTest {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@Test
+	public void listProject() throws JsonParseException, JsonMappingException, IOException, ConfigurationException {
+		ProjectService prjService = new ProjectService();
+		
+		List<Project> prj = prjService.getProjectList();
+		
+		int i = 0;
+		for (Project p : prj) {
+			logger.info(i++ + "th " + p );
+		}
+	}
+	
+	@Test
+	public void getIssue() throws JsonParseException, JsonMappingException, IOException, ConfigurationException {
+		String issueKey = 
+				//"TEST-824";
+				"NCA-208";
+
+		IssueService issueService = new IssueService();
+		Issue issue =  issueService.getIssue(issueKey);
+
+		logger.info(issue.toString());
+	}
+}
