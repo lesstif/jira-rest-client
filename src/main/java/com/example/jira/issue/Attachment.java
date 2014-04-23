5f64c18113e99d860fbd97711c6aaf50f65eff79
lesstif <lesstif@gmail.com>
2014-04-23 17:48:28 +0900

    get Issue completed

---
+++ src/main/java/com/example/jira/issue/Attachment.java
@@ -0,0 +1,15 @@
+package com.example.jira.issue;
+
+import lombok.Data;
+
+@Data
+
+/**
+ * @see https://docs.atlassian.com/jira/REST/latest/#d2e4213
+ * 
+ * @author lesstif
+ *
+ */
+public class Attachment {
+	private String self;
+}
